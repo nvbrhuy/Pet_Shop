c905344589dc3c3b5a0138661afa712851d7f97f
Baohy-14 <nguyenducbaohy22@gmail.com>
2025-11-18 14:51:48 +0700

    admin login

--- projectshopthucung/resources/views/admin_login.blade.php
+++ projectshopthucung/resources/views/admin_login.blade.php
@@ -14,7 +14,7 @@
 
 	<link rel="canonical" href="https://demo-basic.adminkit.io/pages-sign-in.html" />
 
-	<title>Sign In | Admin</title>
+	<title>ĐĂNG NHẬP ADMIN  </title>
 
 	<link href="{{ asset('backend/css/app.css')}}" rel="stylesheet">
 
@@ -22,16 +22,18 @@
 </head>
 
 <body>
-	<main class="d-flex w-100" style="background-image: linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12);">
+	
+
 		<div class="container d-flex flex-column">
 			<div class="row vh-100">
 				<div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
 					<div class="d-table-cell align-middle">
 
 						<div class="text-center mt-4">
-							<h1 class="h2" style="color: white;">Admin Login</h1>
-							<p class="lead" style="color: white;">
-								Sign in to your account to continue
+							<h1 class="mt-3 font-weight-bold text-uppercase text-nowrap">ĐĂNG NHẬP ADMIN <span
+                                            class="text-primary"><b>PETSHOP</b></span></h1>
+							<p class="lead" style="color: red;">
+								Đăng nhập vào tài khoản của bạn để tiếp tục
 							</p>
 						</div>
 
@@ -55,20 +57,20 @@
 									
 										<div class="mb-3">
 											<label class="form-label">Email</label>
-											<input class="form-control form-control-lg" type="text" name="email" placeholder="Enter your email" />
+											<input class="form-control form-control-lg" type="text" name="email" placeholder="Nhập email của bạn" />
 										</div>
 										<div class="mb-3">
-											<label class="form-label">Password</label>
-											<input class="form-control form-control-lg" type="password" name="password" placeholder="Enter your password" />
+											<label class="form-label">Mật Khẩu</label>
+											<input class="form-control form-control-lg" type="password" name="password" placeholder="Nhập mật khẩu của bạn" />
 										</div>
 										<div>
 											<div class="form-check align-items-center">
 												<input id="customControlInline" type="checkbox" class="form-check-input" value="remember-me" name="remember-me">
-												<label class="form-check-label text-small" for="customControlInline">Remember me</label>
+												<label class="form-check-label text-small" for="customControlInline">Lưu đăng nhập</label>
 											</div>
 										</div>
 										<div class="d-grid gap-2 mt-3">
-											<input class="btn btn-lg btn-primary" type="submit" value="Sign in">
+											<input class="btn btn-lg btn-primary" type="submit" value="Đăng nhập">
 										</div>
 									</form>
 								</div>
