2a1f52a876db0566bd04c684666cce76a9f2f544
TranTienDanh-05 <trandanhh27@gmail.com>
2025-12-02 23:45:57 +0700

    dashboard

--- projectshopthucung/resources/views/admin/dashboard.blade.php
+++ projectshopthucung/resources/views/admin/dashboard.blade.php
@@ -26,10 +26,6 @@
                   </div>
                   <h1 class="mt-1 mb-3">{{$totalsOrders}}</h1>
                   <div class="mb-0">
-                    <span class="text-danger">
-                      <i class="mdi mdi-arrow-bottom-right"></i> -2.25%
-                    </span>
-                    <span class="text-muted">Từ tuần trước</span>
                   </div>
                 </div>
               </div>
@@ -53,10 +49,6 @@
                   </div>
                   <h1 class="mt-1 mb-3">{{$totalsCustomer}}</h1>
                   <div class="mb-0">
-                    <span class="text-success">
-                      <i class="mdi mdi-arrow-bottom-right"></i> 5.25%
-                    </span>
-                    <span class="text-muted">Từ tuần trước</span>
                   </div>
                 </div>
               </div>
@@ -80,10 +72,6 @@
                   </div>
                   <h1 class="mt-1 mb-3">{{$totalsSaleProducts}}</h1>
                   <div class="mb-0">
-                    <span class="text-danger">
-                      <i class="mdi mdi-arrow-bottom-right"></i> -3.65%
-                    </span>
-                    <span class="text-muted">Từ tuần trước</span>
                   </div>
                 </div>
               </div>
@@ -107,10 +95,6 @@
                   </div>
                   <h1 class="mt-1 mb-3">{{$totalsMoney}}</h1>
                   <div class="mb-0">
-                    <span class="text-success">
-                      <i class="mdi mdi-arrow-bottom-right"></i> 6.65%
-                    </span>
-                    <span class="text-muted">Từ tuần trước</span>
                   </div>
                 </div>
               </div>
@@ -179,85 +163,6 @@
             </table>
           </div>
         </div>
-
-        <div class="col-12 col-lg-4 col-xxl-3 d-flex">
-          <div class="card flex-fill w-100">
-            <div class="card-header">
-              <h5 class="card-title mb-0">Doanh số hàng tháng</h5>
-            </div>
-            <div class="card-body d-flex w-100">
-              <div class="align-self-center chart chart-lg">
-                <canvas id="chartjs-dashboard-bar"></canvas>
-              </div>
-            </div>
-          </div>
-        </div>
     </div>
-
-    <div style="background-color: #003399;" class="row">
-      <div class="col-12 col-md-6 col-xxl-3 d-flex order-2 order-xxl-3">
-        <div class="card flex-fill w-100">
-          <div class="card-header">
-            <h5 class="card-title mb-0">Sử dụng trình duyệt</h5>
-          </div>
-          <div class="card-body d-flex">
-            <div class="align-self-center w-100">
-              <div class="py-3">
-                <div class="chart chart-xs">
-                  <canvas id="chartjs-dashboard-pie"></canvas>
-                </div>
-              </div>
-
-              <table class="table mb-0">
-                <tbody>
-                  <tr>
-                    <td>Chrome</td>
-                    <td class="text-end">4306</td>
-                  </tr>
-                  <tr>
-                    <td>Firefox</td>
-                    <td class="text-end">3801</td>
-                  </tr>
-                  <tr>
-                    <td>IE</td>
-                    <td class="text-end">1689</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        class="col-12 col-md-12 col-xxl-6 d-flex order-3 order-xxl-2"
-      >
-        <div class="card flex-fill w-100">
-            <div class="card-header">
-                <h5 class="card-title">Biểu đồ đường</h5>
-                <h6 class="card-subtitle text-muted">Biểu đồ đường là cách biểu diễn các điểm dữ liệu trên một đường thẳng.</h6>
-            </div>
-            <div class="card-body">
-                <div class="chart">
-                    <canvas id="chartjs-line"></canvas>
-                </div>
-            </div>
-        </div>		
-      </div>
-      <div class="col-12 col-md-6 col-xxl-3 d-flex order-1 order-xxl-1 ">
-        <div class="card flex-fill">
-          <div class="card-header">
-            <h5 class="card-title mb-0">Lịch</h5>
-          </div>
-          <div class="card-body d-flex">
-            <div class="align-self-center w-100">
-              <div class="chart">
-                <div id="datetimepicker-dashboard"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
   </div>
 @endsection
